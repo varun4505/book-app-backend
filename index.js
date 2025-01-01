93b4528e8a5d69026cd4027310e6fa4d6b3c6d1a
varun4505 <145825700+varun4505@users.noreply.github.com>
2025-01-01 20:30:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use("/api/admin", adminRoutes)
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
-  app.use("/", (req, res) => {
+  app.get("/", (req, res) => {
     res.send("Book Store Server is running!");
   });
 }
